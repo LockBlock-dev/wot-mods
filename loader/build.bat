f3ab64e2af51bdf2aba11a0e48e2bab66b44c27b
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2022-04-21 13:33:52 +0000

    Delete loader directory

--- loader/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-mkdir build\scripts\client\gui\mods
-mkdir build\mods\modules
-
-python2 -m compileall src/.
-
-move src\*.pyc build\scripts\client\gui\mods\
--- loader/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-mkdir -p build/scripts/client/gui/mods
-mkdir -p build/mods/modules
-
-python2 -m compileall src/.
-
-mv src/*.pyc build/scripts/client/gui/mods
